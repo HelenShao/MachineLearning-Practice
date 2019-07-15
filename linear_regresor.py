ec69c104ba5ef32a30006582d5ccf9c5aa5891e2
HelenShao <52927975+HelenShao@users.noreply.github.com>
2019-07-15 13:02:15 -0400

    Update linear_regresor.py

--- linear_regresor.py
+++ linear_regresor.py
@@ -1,20 +1,19 @@
-
 import torch 
 import torch.nn as nn
 import numpy as np
 
-
 x_train = torch.linspace(0,1,steps=500)
-y_train + torch.linspace(0,1,steps=500)
+y_train = 3.0*x_train + 2.0
 
-print(y_train.shape)
+w = torch.randn(1, requires_grad=True. dtype = torch.float, device=device)
+b = torch.randn(1, requires_grad=True. dtype = torch.float, device=device)
 
 ### Linear Regression Model ###
 
 class Model(torch.nn.Module):
 	def __init__(self):
-		super().__init__()
-		self.linear = torch.nn.Linear(1,1)
+        super(Model,self).__init__()
+		self.linear = torch.nn.Linear(500,2)
 	def forward(self,x):
 		y_pred = self.linear(x)
 		return y_pred
@@ -24,16 +23,23 @@ model = Model()
 criterion = nn.MSELoss() #Loss Function
 optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
 
+
 ### Training ##
 
-for epoch in range(100):
-	y_pred = model(x_train)
-	print(y_pred.shape)
-	print(y_train.shape)
-	loss = criterion(y_pred, y_train)
-	optimizer.zero_grad()
-	loss.backward()
-	optimizer.step()
+for epoch in range(3000):
+	y_pred = model(x_train.view(-1,1))  #Change the shape of the input to 1 
+	loss = criterion(y_pred, y_train.view(-1,1))
+   	loss_tensor = torch.tensor(loss)
+   	print(epoch, loss_tensor.item())
+	optimizer.zero_grad()  #Remember the gradient!
+	loss.backward()        #Gradient 
+	optimizer.step()       #Update parameters
+   	weights = list(model.parameters())
+   	print(weights)
+ 
+### Validation ###
+test_point = torch.FloatTensor([[4.0]])
+print(model(test_point).data[0][0].item())
 	
 
 	
