0a72568c368c6cf8b43ef5f34dfd5d0c3cc406fe
HelenShao <52927975+HelenShao@users.noreply.github.com>
2019-07-15 13:43:09 -0400

    Update param_prediction.py

--- param_prediction.py
+++ param_prediction.py
@@ -7,31 +7,28 @@ y_train = 3.0*x_train + 2.0
 
 params_train = torch.tensor([3.0, 2.0])
 
-w = torch.randn(1, requires_grad=True)
-b = torch.randn(1, requires_grad=True)
-
 ### Linear Regression Model ###
 
-class Model(torch.nn.Module):
+class Model(torch.nn.Module):    # Use the Linear Torch Module
     def __init__(self):
         super(Model,self).__init__()
-        self.linear = torch.nn.Linear(500,2)
+        self.linear = torch.nn.Linear(500,2)    # Take in 500 points, return 2 parameters
     def forward(self,x):
         params_pred = self.linear(x)
-        return params_pred
+        return params_pred            # Predict the parameters
 
 model = Model()
 criterion = nn.MSELoss() #Loss Function
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.0001)
+optimizer = torch.optim.SGD(model.parameters(), lr = 0.0001)   #Learning rate is extremely small in this case
 
-    ### Training ##
+## Training ##
 
 for epoch in range(3000):
-    params_pred = model(y_train)
-    loss = criterion(params_pred, params_train)
+    params_pred = model(y_train)                 # Input is y_train (500 points), using linear model
+    loss = criterion(params_pred, params_train)  # Calculates Loss based on params_pred and params_train
     loss_tensor = torch.tensor(loss)
     print(epoch, loss_tensor.item())
     optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    loss.backward()         #Gradient
+    optimizer.step()        #Update parameters
     print(params_pred)
