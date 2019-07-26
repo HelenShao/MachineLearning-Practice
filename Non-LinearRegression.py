efa9ae3cd796e870f4082814c68713f6c9f6748c
HelenShao <52927975+HelenShao@users.noreply.github.com>
2019-07-25 21:54:40 -0400

    Update Non-LinearRegression.py

--- Non-LinearRegression.py
+++ Non-LinearRegression.py
@@ -33,56 +33,6 @@ plt.xlabel('X_Train')
 plt.ylabel('Y_Train')
 plt.show()
 
-
-# Create Model Container
-Model = nn.Sequential()
-
-# Define Layers
-Linear_Layer = nn.Linear(1,100)       # Add many neuronsin hidden layers
-Relu1 = nn.ReLU(inplace=False)
-Linear_Layer2 = nn.Linear(100,200)
-Relu2 = nn.ReLU(inplace=False)
-Linear_Layer3 = nn.Linear(200,1)
-
-
-# Add layers to model
-
-Model.add_module("Lin1", Linear_Layer)
-Model.add_module("Relu1", Relu1)              #Activation for first linear
-Model.add_module("Lin2", Linear_Layer2)
-Model.add_module("Relu2", Relu2)              #Activation for second linear
-Model.add_module("Lin3", Linear_Layer3)
-
-criterion = nn.MSELoss()       #Loss Function      
-optimizer = torch.optim.SGD(Model.parameters(), lr = 0.0001) 
-
-### Training ###
-
-loss_total = np.zeros(5000)
-Model.train()
-
-for epoch in range(5000):
-    y_pred = Model(x_train.view(-1,1))
-    loss = criterion(y_pred, y_train.view(-1,1))
-    loss_total[epoch] = loss.detach().numpy()
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    #Print Loss
-    loss_tensor = torch.tensor(loss)
-    print('epoch: ', epoch, 'loss: ', loss_tensor.item())
-
-
-### Plot loss as a function of epochs ###
-
-epochs = np.arange(5000)
-plt.plot(epochs, loss_total)
-plt.ylabel("loss")
-plt.xlabel("epochs")
-plt.yscale('log')
-plt.xscale('log')
-
 # Test Set
 
 x_test = torch.randn(5000)
@@ -103,26 +53,59 @@ print(y_testn)
 plt.scatter(torch.Tensor.numpy(x_testn), torch.Tensor.numpy(y_testn))
 plt.show()
 
-### Validation ###
 
-Model.eval() #Set model into evaluation mode
+# Create Model Container
+Model = nn.Sequential()
 
-error_total = np.zeros(5000)
+# Define Layers
+Linear_Layer = nn.Linear(1,100)       # Add many neuronsin hidden layers
+Relu1 = nn.ReLU(inplace=False)
+Linear_Layer2 = nn.Linear(100,200)
+Relu2 = nn.ReLU(inplace=False)
+Linear_Layer3 = nn.Linear(200,1)
+
+
+# Add layers to model
+
+Model.add_module("Lin1", Linear_Layer)
+Model.add_module("Relu1", Relu1)              #Activation for first linear
+Model.add_module("Lin2", Linear_Layer2)
+Model.add_module("Relu2", Relu2)              #Activation for second linear
+Model.add_module("Lin3", Linear_Layer3)
+
+
+criterion = nn.MSELoss()       #Loss Function           
+optimizer1 = torch.optim.SGD(Model.parameters(), lr = 0.001) 
+optimizer2 = torch.optim.SGD(Model.parameters(), lr = 0.001) 
+
+
+### Training ###
+loss_total = np.zeros(5000) # Loss for training set
+error_valid_total = np.zeros(5000) # Loss for validation set
+
+Model.train()
 
 for epoch in range(5000):
-    y_prediction = Model(x_testn.view(-1,1))
-    error = criterion(y_prediction, y_testn.view(-1,1))
-    error_total[epoch] = error.detach().numpy()
-    optimizer.zero_grad()
-    error.backward()
-    optimizer.step()
-
-print(y_prediction, error_total)
+    y_pred = Model(x_train.view(-1,1))
+    loss = criterion(y_pred, y_train.view(-1,1))     #Loss function for train set
+    loss_total[epoch] = loss.detach().numpy()
+    optimizer1.zero_grad()
+    loss.backward()
+    optimizer1.step()
     
-epochs = np.arange(5000)
-plt.plot(epochs, error_total)
-plt.show()
-
+    y_validation = Model(x_testn.view(-1,1))
+    error_valid = criterion(y_validation, y_testn.view(-1,1))   #Loss function for validation set
+    error_valid_total[epoch] = error_valid.detach().numpy()
+    optimizer2.zero_grad()
+    error_valid.backward()
+    optimizer2.step()
+    
+    #Print Loss for both
+    loss_tensor = torch.tensor(loss)
+    error_valid_tensor = torch.tensor(error_valid)
+    print('epoch: ', epoch, 'loss: ', loss_tensor.item(), '  error:', error_valid_tensor.item())
+    
+    
 # Plot Training Loss and Validation Error
 
 plt.ylabel('error')
@@ -132,3 +115,20 @@ plt.xscale('log')
 plt.plot(epochs, error_total)
 plt.plot(epochs, loss_total)
 plt.show()
+
+# Undo Normalization for y_prediction
+y_prediction = y_prediction * (torch.max(y_prediction) - torch.min(y_prediction)) + torch.tensor(y_prediction)
+
+#Plot prediction and training set
+plt.ylabel('Y')
+plt.xlabel('X')
+plt.plot(x, y_prediction)   #Not normalized x-values and y_prediction
+
+#Undo Normalization for y_validation
+y_validation = y_validation * (torch.max(y_valiadtion) - torch.min(y_validation)) + torch.tensor(y_validation)
+
+#Plot prediction and validation set
+plt.ylabel('Y')
+plt.xlabel('X')
+plt.plot(x_test, y_validation)
+
